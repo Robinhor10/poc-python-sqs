8c202c30a554fe16792824dfcc022048a5fd97b6
Robinhor10 <robinhopj@gmail.com>
2024-03-09 16:29:46 -0300

    refatoracao do projeto

--- setup.js
+++ setup.js
@@ -1,85 +1,48 @@
 const AWS = require('aws-sdk');
 
-const dynamoDB = new AWS.DynamoDB({
+AWS.config.update({
   region: 'us-east-1',
-  endpoint: "http://localhost:4566",
+  endpoint: 'http://localstack:4566',
   accessKeyId: 'test',
-  secretAccessKey: 'test',
-}) ;
+  secretAccessKey: 'test'
+});
+
+const dynamoDB = new AWS.DynamoDB();
 
 const tables = [
-    {
-        TableName :  "Reservations",
-        KeySchema : [
-            {
-                AttributeName: "id",
-                KeyType: "HASH",
-            }
-        ],
-        AttributeDefinitions: [
-            {
-                AttributeName: "id",
-                AttributeType: "S",
-            }
-        ],
-        ProvisionedThroughput: {
-            ReadCapacityUnits: 1,
-            WriteCapacityUnits: 1
-        }
-    },
-    {
-        TableName :  "Payments",
-        KeySchema : [
-            {
-                AttributeName: "paymentid",
-                KeyType: "HASH",
-            }
-        ],
-        AttributeDefinitions: [
-            {
-                AttributeName: "paymentid",
-                AttributeType: "S",
-            }
-        ],
-        ProvisionedThroughput: {
-            ReadCapacityUnits: 1,
-            WriteCapacityUnits: 1
-        }
-    },
-    {
-        TableName :  "Notifications",
-        KeySchema : [
-            {
-                AttributeName: "notificationid",
-                KeyType: "HASH",
-            }
-        ],
-        AttributeDefinitions: [
-            {
-                AttributeName: "notificationid",
-                AttributeType: "S",
-            }
-        ],
-        ProvisionedThroughput: {
-            ReadCapacityUnits: 1,
-            WriteCapacityUnits: 1
-        }
-    },    
+  {
+    TableName: 'Reservations',
+    KeySchema: [{ AttributeName: 'reservationId', KeyType: 'HASH' }],
+    AttributeDefinitions: [{ AttributeName: 'reservationId', AttributeType: 'S' }],
+    ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 }
+  },
+  {
+    TableName: 'Payments',
+    KeySchema: [{ AttributeName: 'paymentId', KeyType: 'HASH' }],
+    AttributeDefinitions: [{ AttributeName: 'paymentId', AttributeType: 'S' }],
+    ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 }
+  },
+  {
+    TableName: 'Notifications',
+    KeySchema: [{ AttributeName: 'notificationId', KeyType: 'HASH' }],
+    AttributeDefinitions: [{ AttributeName: 'notificationId', AttributeType: 'S' }],
+    ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 }
+  }
 ];
 
-async function createTable(tables) {
-    try {
-        const result = await dynamoDB.createTable(table).promise();
-        console.log('Tabela criada:  ${result.TableDescription.TableName}' );
-    } catch (error) {
-        console.error('Erro ao criar a tabela:  ${error.message}' );
-    }
+async function createTable(table) {
+  try {
+    const result = await dynamoDB.createTable(table).promise();
+    console.log(`Table created: ${result.TableDescription.TableName}`);
+  } catch (error) {
+    console.error(`Error creating table: ${error}`);
+  }
 }
 
-async function setup() {
-    for (let table of tables) {
-        await createTable(table);
-    }
+async function setupTables() {
+  for (const table of tables) {
+    await createTable(table);
+  }
 }
 
-setup();
+setupTables();
